28171d09a53318a9291b17b8e8241828bfb98a2c
Vinicius-Zanetti05 <vin.zanetti05@gmail.com>
2025-06-04 19:36:26 +0000

    Adicionei a Carta 1 com entrada e exibição de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 int main() {
     
   char estado1, estado2;
@@ -11,6 +9,24 @@ int main() {
   float area1, area2;
   float pib1, pib2;
   int pontos1, pontos2;
+
+  printf("Carta 1:\n");
+  printf("Estado (A-H): ");
+  scanf(" %c", &estado1); 
+  printf("Codigo da Carta (ex: A01): ");
+  scanf("%s", codigo1);
+  printf("Nome da Cidade: ");
+  scanf(" %[^\n]", cidade1); 
+  printf("Populacao: ");
+  scanf("%d", &populacao1);
+  printf("Area (em km2): ");
+  scanf("%f", &area1);
+  printf("PIB (em bilhoes de reais): ");
+  scanf("%f", &pib1);
+  printf("Numero de Pontos Turisticos: ");
+  scanf("%d", &pontos1);
+
+  printf("\n");
 
     return 0;
 }
