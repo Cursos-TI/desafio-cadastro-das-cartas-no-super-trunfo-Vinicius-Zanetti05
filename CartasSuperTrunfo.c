6b9b08712f504df82c48d34234c7942892caa954
Vinicius-Zanetti05 <vin.zanetti05@gmail.com>
2025-06-04 19:41:20 +0000

    Finalizado o cadastro e a exibição das duas cartas do Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -46,5 +46,27 @@ int main() {
 
   printf("\n");
 
+   printf("=== Cartas Cadastradas ===\n\n");
+
+  printf("Carta 1:\n");
+  printf("Estado: %c\n", estado1);
+  printf("Codigo: %s\n", codigo1);
+  printf("Nome da Cidade: %s\n", cidade1);
+  printf("Populacao: %d\n", populacao1);
+  printf("Area: %.2f km2\n", area1);
+  printf("PIB: %.2f bilhoes de reais\n", pib1);
+  printf("Numero de Pontos Turisticos: %d\n", pontos1);
+
+  printf("\n");
+
+  printf("Carta 2:\n");
+  printf("Estado: %c\n", estado2);
+  printf("Codigo: %s\n", codigo2);
+  printf("Nome da Cidade: %s\n", cidade2);
+  printf("Populacao: %d\n", populacao2);
+  printf("Area: %.2f km2\n", area2);
+  printf("PIB: %.2f bilhoes de reais\n", pib2);
+  printf("Numero de Pontos Turisticos: %d\n", pontos2);
+
     return 0;
 }
